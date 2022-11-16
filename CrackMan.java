9d995958c2fe1ddc54f99309229092fc900fb1a6
Codenameomega51 <youssefabdelnour2004@yahoo.com>
2022-11-15 21:23:50 -0500

    Moving, rotation, eating for crackman

--- CrackMan.java
+++ CrackMan.java
@@ -1,3 +1,4 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
 import lang.stride.*;
 import java.util.*;
 import greenfoot.*;
@@ -14,5 +15,50 @@ public class CrackMan extends Actor
      */
     public void act()
     {
+        moving();
+        eatTacos();
+        eatPowerup();
+    }
+
+    /**
+     * 
+     */
+    public void moving()
+    {
+        move(2);
+        if (Greenfoot.isKeyDown("w")) {
+            setRotation(270);
+        }
+        if (Greenfoot.isKeyDown("s")) {
+            setRotation(90);
+        }
+        if (Greenfoot.isKeyDown("a")) {
+            setRotation(180);
+        }
+        if (Greenfoot.isKeyDown("d")) {
+            setRotation(0);
+        }
+    }
+
+    /**
+     * 
+     */
+    public void eatTacos()
+    {
+        Actor taco = getOneIntersectingObject(Tacos.class);
+        if (taco != null) {
+            getWorld().removeObject(taco);
+        }
+    }
+
+    /**
+     * 
+     */
+    public void eatPowerup()
+    {
+        Actor line = getOneIntersectingObject(Powerup.class);
+        if (line != null) {
+            getWorld().removeObject(line);
+        }
     }
 }
