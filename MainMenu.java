77950a4be8a60d7d5b7ede61e8c782a06c919159
Codenameomega51 <youssefabdelnour2004@yahoo.com>
2022-11-19 12:14:35 -0500

    minor changes to heart and main menu world

--- MainMenu.java
+++ MainMenu.java
@@ -24,31 +24,31 @@ public class MainMenu extends World
      */
     private void prepare()
     {
-        Ghost1 ghost1 =  new  Ghost1();
+        Ghost1 ghost1 =  new Ghost1();
         addObject(ghost1, 503, 558);
-        CrackMan crackMan =  new  CrackMan();
+        CrackMan crackMan =  new CrackMan();
         addObject(crackMan, 190, 351);
-        Tacos tacos =  new  Tacos();
+        Tacos tacos =  new Tacos();
         addObject(tacos, 660, 172);
-        Tacos tacos2 =  new  Tacos();
+        Tacos tacos2 =  new Tacos();
         addObject(tacos2, 708, 244);
-        Tacos tacos3 =  new  Tacos();
+        Tacos tacos3 =  new Tacos();
         addObject(tacos3, 592, 286);
-        Tacos tacos4 =  new  Tacos();
+        Tacos tacos4 =  new Tacos();
         addObject(tacos4, 662, 392);
-        Tacos tacos5 =  new  Tacos();
+        Tacos tacos5 =  new Tacos();
         addObject(tacos5, 770, 355);
-        Tacos tacos6 =  new  Tacos();
+        Tacos tacos6 =  new Tacos();
         addObject(tacos6, 769, 491);
-        Tacos tacos7 =  new  Tacos();
+        Tacos tacos7 =  new Tacos();
         addObject(tacos7, 562, 554);
-        Tacos tacos8 =  new  Tacos();
+        Tacos tacos8 =  new Tacos();
         addObject(tacos8, 748, 618);
-        Tacos tacos9 =  new  Tacos();
+        Tacos tacos9 =  new Tacos();
         addObject(tacos9, 733, 548);
-        Tacos tacos10 =  new  Tacos();
+        Tacos tacos10 =  new Tacos();
         addObject(tacos10, 660, 489);
-        Tacos tacos11 =  new  Tacos();
+        Tacos tacos11 =  new Tacos();
         addObject(tacos11, 791, 447);
         removeObject(ghost1);
     }
